f6f9fb41369882a189e142c0db58eeb7ba7097d7
Sergio Roma <roma.sergio@gmail.com>
2019-02-15 01:34:27 +0000

    Added a directive for the dropdown menus

---
+++ src/app/shared/dropDow.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, HostListener, Input, TemplateRef, ViewContainerRef, HostBinding } from '@angular/core';
+
+@Directive({
+    selector: '[appDropdown]'
+})
+
+export class DropDownDirective {
+
+    @HostBinding('class.open') isOpen: boolean = false;
+    constructor( 
+        //private temRef: TemplateRef<any>,
+        //private vcRef: ViewContainerRef  
+    ){}
+
+   
+
+    @HostListener('click') toggleOpen(evenData: Event ){
+        console.log('this is from the directive.');
+        this.isOpen = !this.isOpen;
+    }
+}
